77e9bb370a31f8f92e240400674075a7f6711a5a
prajakta <prajaktamahajan789@gmail.com>
2021-09-26 15:41:04 +0530

    Display revarse number

---
+++ RevarseNumber.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.Logical_Programe;
+
+import java.util.*;
+
+public class RevarseNumber {
+
+
+	public static void main(String args[])
+	   {
+	      int num=0;
+	      int reversenum =0;
+	      System.out.println("Input your number and press enter: ");
+	      
+	      Scanner in = new Scanner(System.in);
+	      
+	      num = in.nextInt();
+	     
+	      while( num != 0 )
+	      {
+	          reversenum = reversenum * 10;
+	          reversenum = reversenum + num%10;
+	          num = num/10;
+	      }
+
+	      System.out.println("Reverse of input number is: "+reversenum);
+	   }
+	}
